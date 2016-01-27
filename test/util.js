32a8224334c02416af57a8c877ae7e7b4d18926d
Javier Collado <javier.collado@gmail.com>
2016-01-27 09:37:47 +0100

    refactor(test): Refactor util.isFile test cases to reuse configuration code

--- test/util.js
+++ test/util.js
@@ -4,41 +4,38 @@ const requireInject = require('require-inject')
 const sinon = require('sinon')
 const test = require('ava').test
 
-test('isFile: returns true if it is a file', function (t) {
-  const stubs = {
+function isFileStubs (stat) {
+  return {
     fs: {
-      stat: sinon.stub().yields(null, {
-        isFile: sinon.stub().returns(true)
-      })
+      stat
     }
   }
-  const util = requireInject('../src/util', stubs)
+}
+
+test('isFile: returns true if it is a file', function (t) {
+  const stat = sinon.stub().yields(null, {
+    isFile: sinon.stub().returns(true)
+  })
+
+  const util = requireInject('../src/util', isFileStubs(stat))
   return util.isFile('some file').then(function (isFile) {
     t.true(isFile)
   })
 })
 
 test('isFile: returns false if is not a file', function (t) {
-  const stubs = {
-    fs: {
-      stat: sinon.stub().yields(null, {
-        isFile: sinon.stub().returns(false)
-      })
-    }
-  }
-  const util = requireInject('../src/util', stubs)
+  const stat = sinon.stub().yields(null, {
+    isFile: sinon.stub().returns(false)
+  })
+  const util = requireInject('../src/util', isFileStubs(stat))
   return util.isFile('some file').then(function (isFile) {
     t.false(isFile)
   })
 })
 
 test('isFile: returns false on error', function (t) {
-  const stubs = {
-    fs: {
-      stat: sinon.stub().yields(new Error('some error'))
-    }
-  }
-  const util = requireInject('../src/util', stubs)
+  const stat = sinon.stub().yields(new Error('some error'))
+  const util = requireInject('../src/util', isFileStubs(stat))
   return util.isFile('some file').then(function (isFile) {
     t.false(isFile)
   })
